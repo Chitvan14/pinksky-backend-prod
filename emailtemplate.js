935c94b9c1248d3c94a685d16654937a41be50f7
Chitvan14 <chitvangarg14@gmail.com>
2023-02-08 19:07:31 +0530

    mail check

--- emailtemplate.js
+++ emailtemplate.js
@@ -13,7 +13,7 @@ module.exports = function (type, data) {
     type === "verifyemail"
   ) {
     // html = `<span> ${data.text} </span> <a href=${data.href} style="text-decoration:none; color:#151515;"> Go to website →</a>`;
-    html = `<html>
+    html = `<!DOCTYPE html><html>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
     </style>
@@ -44,7 +44,6 @@ module.exports = function (type, data) {
                                                         <div style="display:flex;text-align:center;flex-direction:column;max-width:358px;padding-top:12px;vertical-align:top;margin:auto">
                                                             <p style="font-family:Lato;text-align:left;font-weight:400;font-style:normal;font-size:13px;line-height:24px;margin:auto;color:#151515">${data.text}</p>
                                                         </div>
-                                                        <a href=${data.href} target="_blank" style="font-family:Lato;font-weight:400;font-size:13px;line-height:20px;text-align:center;letter-spacing:.25px;color:#151515;padding:6px 16px;background:#f7f7f7;border-radius:6px;margin:auto;margin-top:14px;display:block;width:200px;text-decoration:none">${data.hrefText}</a>
                                                     </td>
                                                 </tr>
                                             </table>
@@ -75,9 +74,6 @@ module.exports = function (type, data) {
                                                         <p style="margin:auto;margin-bottom:4px;font-weight:400;font-size:13px;line-height:28.8px;text-align:center;color:#151515;font-family:Lato">If you have any questions, please reach out to us at</p>
                                                         <a href="mailto:pinkskyclubdev@gmail.com" target="_blank" style="font-family:Lato;font-weight:400;font-size:13px;line-height:20px;text-align:center;letter-spacing:.25px;color:#151515;padding:6px 16px;background:#f7f7f7;border-radius:6px;margin:auto;margin-bottom:14px;display:block;width:200px;text-decoration:none">Email</a>
                                                         <a href="https://pinkskyclub.com/contact" target="_blank" style="font-family:Lato;font-weight:400;font-size:13px;line-height:20px;text-align:center;letter-spacing:.25px;color:#151515;padding:6px 16px;background:#f7f7f7;border-radius:6px;margin:auto;margin-bottom:14px;display:block;width:200px;text-decoration:none">Contact</a>
-                                                        <div style="filter: invert(100%);margin:0 32px 24px;text-align:center"><img src="https://firebasestorage.googleapis.com/v0/b/pinksky-8804c.appspot.com/o/brand-logo%2Fwis-line.png?alt=media&token=2c641a8a-f4f4-4616-abde-00a53de700a3"></div>
-    
-                                                        </div>
                                                     </td>
                                                 </tr>
                                             </table>
