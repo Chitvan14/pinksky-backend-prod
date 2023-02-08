9b23847ac38c4b87982a912966021a51e289c64c
Chitvan14 <chitvangarg14@gmail.com>
2023-02-08 22:05:15 +0530

    8-02-2023

--- emailtemplate.js
+++ emailtemplate.js
@@ -13,7 +13,12 @@ module.exports = function (type, data) {
     type === "verifyemail"
   ) {
     // html = `<span> ${data.text} </span> <a href=${data.href} style="text-decoration:none; color:#151515;"> Go to website →</a>`;
-    html = `<html>
+    html = `<!doctype html><html> <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Email</title>
+  </head>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
     </style>
